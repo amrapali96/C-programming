fb81b78659c1cb6b30d546300cc1cedfbe9c2f72
amrapali96 <amrapalisbhise@gmail.com>
2021-03-27 18:20:42 +0530

    Day 2: Added Text File

--- producer_consumer/producer/main.c
+++ producer_consumer/producer/main.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdint.h>
+
 int main(void)
 {
+    
     fprintf(stdout,"In %s file, at %d line", __FILE__, __LINE__); 
     //Todo
+    // get file name
+    // check if there exist a file
+    // open file -- readonly
+    // read x number bytes into buffer
+    
 
 
     fprintf(stdout, "\n\n");
